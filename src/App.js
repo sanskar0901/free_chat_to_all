eb0fff19fe3ba76a5999567fb105c61e01b6d7d8
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2021-03-30 17:33:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1 className="chatfull">CHAT FULL</h1>
         <SignOut />
       </header>
 
@@ -105,7 +105,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      <button type="submit" disabled={!formValue}>⬆️</button>
 
     </form>
   </>)
